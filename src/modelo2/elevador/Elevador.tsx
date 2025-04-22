7e65112435d3f2b274401efdb976a08e2c62eb5c
Silas Rodrigues Nascimento - 82327338 <82327338@ulife.com.br>
2025-04-22 20:12:59 -0300

    backend final

--- src/modelo2/elevador/Elevador.tsx
+++ src/modelo2/elevador/Elevador.tsx
@@ -3,8 +3,8 @@ import "./Cabin.css"
 import { useState } from "react";
 
 function Elevador() {
-  let esperaBasica = 4000;
-  let esperaTudo = 12000;
+  let esperaBasica = 500;
+  //let esperaTudo = 1000;
 
   let andar3 = 132;
   let andar2 = 291;
@@ -23,7 +23,8 @@ function Elevador() {
   const [currentFloor, setCurrentFloor] = useState(0);
   const [positionY, setPositionY] = useState(andar0);
 
-  const [porta, setPorta] = useState(false);
+  let porta = false;
+  //const [porta, setPorta] = useState(false);
   const [p, setP] = useState(100);
 
   async function mudarAdar(atual: number) {
@@ -82,47 +83,65 @@ function Elevador() {
     }
   }
 
-
+  let realy = false
   async function changePosition(selecionado: number) {
-    if (porta && operation) {
-      if (selecionado > currentFloor) {
-        console.log("subindo");
-        for (let i = currentFloor; i <= selecionado;) {
-          mudarAdar(i);
-          if (i != currentFloor) {
-            console.log("espera antes de subir 1");
-            await resolveAfter2Seconds(esperaTudo);
-          } else { console.log("espera antes de subir quando for igual ao andar atual"); await resolveAfter2Seconds(esperaBasica); }
-          i++;
-        }
+    realy = true;
+
+      if (porta) {
+        if (selecionado > currentFloor) {
+          console.log("subindo");
+          mudarAdar(selecionado);
+          realy = false;
+          // for (let i = currentFloor; i <= selecionado;) {
+          //   if (i != currentFloor) {
+          //     console.log("espera antes de subir 1");
+
+          //   } else { console.log("espera antes de subir quando for igual ao andar atual"); await resolveAfter2Seconds(esperaBasica); }
+          //   i++;
+          // }
 
 
-      } else if (selecionado < currentFloor) {
-        console.log("descendo");
-        for (let i = currentFloor; i >= selecionado;) {
-          mudarAdar(i);
-          if (i != currentFloor) {
-            console.log("espera antes de descer 1");
-            await resolveAfter2Seconds(esperaTudo);
-          } else { console.log("espera antes de descer 1"); await resolveAfter2Seconds(esperaBasica); }
-          i--;
-          console.log("proximo valor de i = " + i);
+        } else if (selecionado < currentFloor) {
+          console.log("descendo");
+          mudarAdar(selecionado);
+          realy = false;
+          // for (let i = currentFloor; i >= selecionado;) {
+          //   if (i != currentFloor) {
+          //     console.log("espera antes de descer 1");
+          //     await resolveAfter2Seconds(esperaTudo);
+          //   } else { console.log("espera antes de descer 1"); await resolveAfter2Seconds(esperaBasica); }
+          //   i--;
+          //   console.log("proximo valor de i = " + i);
+          // }
+        } else {
+          console.log("é o mesmo andar");
         }
       } else {
-        console.log("é o mesmo andar");
+        console.log("porta aberta !!");
       }
-    } else console.log("porta aberta !!");
-
   }
 
-  async function fecharPorta() {
+  async function fecharPorta( selecionado : number) {
+    if (!porta) {
+      setOperation(false)
+      setP(0);
+      console.log("espera fechar a porta");
+      await resolveAfter2Seconds(500);
+      console.log("fechou");
+      porta =true;
+      setOperation(true);
+      changePosition(selecionado);
+    }
+    console.log("################################################################################################");
+  }
+  async function fecharPort() {
     if (operation) {
       setOperation(false)
       setP(0);
       console.log("espera fechar a porta");
-      await resolveAfter2Seconds(2000);
+      await resolveAfter2Seconds(500);
       console.log("fechou");
-      setPorta(true);
+      porta =true;
       setOperation(true);
     }
     console.log("################################################################################################");
@@ -134,7 +153,7 @@ function Elevador() {
     console.log("espera abrir a porta");
     await resolveAfter2Seconds(esperaBasica);
     console.log("abriu");
-    setPorta(false);
+    porta = false;
     setOperation(true);
   }
 
@@ -144,9 +163,9 @@ function Elevador() {
       <div id="tudo">
         <div id="space">
           <div id="predio">
-              <div id="cabin" style={{ top: positionY }}>
-                <div id="door" style={{ width: p }}></div>
-              </div>
+            <div id="cabin" style={{ top: positionY }}>
+              <div id="door" style={{ width: p }}></div>
+            </div>
             <div>
               <div id="f3" className="floor"></div>
               <div id="f2" className="floor"></div>
@@ -157,12 +176,12 @@ function Elevador() {
           <div id="chao"></div>
         </div>
         <div id="controller">
-          <div id="btn" onClick={() => { changePosition(3) }}><h2>3</h2></div>
-          <div id="btn" onClick={() => { changePosition(2) }}><h2>2</h2></div>
-          <div id="btn" onClick={() => { changePosition(1) }}><h2>1</h2></div>
-          <div id="btn" onClick={() => { changePosition(0) }}><h2>T</h2></div>
+          <div id="btn" onClick={() => { fecharPorta(3) }}><h2>3</h2></div>
+          <div id="btn" onClick={() => { fecharPorta(2) }}><h2>2</h2></div>
+          <div id="btn" onClick={() => { fecharPorta(1) }}><h2>1</h2></div>
+          <div id="btn" onClick={() => { fecharPorta(0) }}><h2>T</h2></div>
           <div id="btn" onClick={() => { abrirPorta() }}><h2>{`<|>`}</h2></div>
-          <div id="btn" onClick={() => { fecharPorta() }}><h2>{`>|<`}</h2></div>
+          <div id="btn" onClick={() => { fecharPort() }}><h2>{`>|<`}</h2></div>
         </div>
       </div>
     </>
